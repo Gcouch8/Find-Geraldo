8c14d5b2d54c9e314972768965083bcdb3db43f0
GARETH COUCH <gcouch8@gmail.com>
2015-11-05 21:00:07 -0800

    add variables and functions

---
+++ script.js
@@ -0,0 +1,37 @@
+var images=['House', 'Tree', 'Mountain', 'Desert', 'Ocean', 'Fish'];
+var userAnswer=prompt('Where is the human hiding?');
+var answer='the fish';
+
+findTheHuman();
+
+function findTheHuman(){
+  if((userAnswer===answer)||(userAnswer==='the Fish')||(userAnswer==='Fish')||(userAnswer==='fish')){
+    alert('You found him! Incredible');
+    document.getElementById('imageSix').style.borderStyle='solid';
+    document.getElementById('success').innerHTML='You are a hero of the people';
+  }
+  else{
+    alert('Nope he is not there.');
+    var userAnswerTwo = prompt('Guess Again');
+
+    if ((userAnswerTwo===answer)||(userAnswer==='the Fish')||(userAnswer==='Fish')||(userAnswer==='fish')){
+      alert('Nice job, you found him!');
+      document.getElementById('imageSix').style.borderStyle='solid';
+      document.getElementById('success').innerHTML='You are a hero of the people';
+    }
+    else{
+      alert('Nope, only one more try! Before he vanishes for good!');
+      var userAnswerThree = prompt('Guess Again');
+      if ((userAnswerThree===answer)||(userAnswer==='the Fish')||(userAnswer==='Fish')||(userAnswer==='fish')){
+        alert('Phew, you found him before he escaped.');
+        document.getElementById('imageSix').style.borderStyle='solid';
+        document.getElementById('success').innerHTML='You are a hero of the people';
+      }
+      else{
+        alert('Geraldo has escaped to continue his nefarious ways.');
+        document.getElementById('fail').innerHTML='How could you let a dangerous criminal escape?';
+      }
+    }
+   }
+ };
+
